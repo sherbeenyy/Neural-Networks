ae9291ac41fc5822156e8b58be044812929e75e6
Ahmed Hesham <A.Ahmed06380@student.aast.edu>
2024-07-03 17:55:06 +0300

    feed forward

--- Program.cs
+++ Program.cs
@@ -3,12 +3,6 @@ class Program
 {
     static void Main()
     {
-        int [] layers = [2, 3, 2]; // 2 input nodes, 3 hidden nodes, 1 output node
-        Network n = new Network(layers);  
-        n.display_weights();
-
-        Console.WriteLine("Bias:");
-        n.display_bias();
         
 
     }
--- network.cs
+++ network.cs
@@ -27,7 +27,40 @@ public Network(int numOfInputs, int numOfHidden, int numOfOutputs){
     }
 }
 
+private double sigmoid(double function)
+{
+    return 1 / (1 + Math.Exp(-function));
+}
+
+private double sigmoid_derivative(double function)
+{
+    return function * (1 - function);
+}
+
+private double reLU(double function)
+{
+    return Math.Max(0, function);
+}
+
+private double reLU_derivative(double function)
+{
+    return function > 0 ? 1 : 0;
+}
 
 
-
+public double[] feedForward(double[] inputs)
+{
+    
+    for (int i = 0; i < hidden_layer.Length; i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < inputs.Length; j++)
+        {
+            sum += inputs[j] * hidden_layer[i].Weights[j];
+        }
+        sum += hidden_layer[i].Bias;
+        hidden_layer[i].Output = sigmoid(sum);
+    }
+    return hidden_layer.Select(neuron => neuron.Output).ToArray();
+}
 }
